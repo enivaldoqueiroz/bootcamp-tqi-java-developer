b0cbb05160f1c5ec29c7195989f38d141340913f
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-15 18:01:27 -0300

    POO

--- src/poo/modal/Cliente.java
+++ src/poo/modal/Cliente.java
@@ -1,5 +1,12 @@
 package poo.modal;
 
+import java.util.List;
+
 public class Cliente {
 
+	public Integer codigo;
+	public String nome;
+	public String cpf;
+	
+	public List<Endereco> enderecos;
 }
