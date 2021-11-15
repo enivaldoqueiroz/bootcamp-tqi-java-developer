0f16914b92193fce59b386bd0407da30f4dc31eb
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-15 18:28:49 -0300

    Implementando encapsulamento na classe Cliente

---
+++ src/poo/EntregaCartaoApp.java
@@ -0,0 +1,27 @@
+package poo;
+
+import java.util.ArrayList;
+
+import poo.modal.Cliente;
+import poo.modal.Endereco;
+
+public class EntregaCartaoApp {
+
+	public static void main(String[] args) {
+
+		Endereco endereco = new Endereco();
+		endereco.cep = "000000000";
+		// dados do endereco
+		Cliente cliente = new Cliente();
+		// dados do cliente
+		
+		try {
+			cliente.adicionaEndereco(endereco);
+			System.out.println("Endereco adicionado com sucesso!");
+			
+		} catch (Exception e) {
+			System.err.println("Houve um erro ao adicionar endereco!" + e.getMessage());
+		}
+	}
+
+}
--- src/poo/modal/Cliente.java
+++ src/poo/modal/Cliente.java
@@ -1,5 +1,6 @@
 package poo.modal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
@@ -8,5 +9,28 @@ public class Cliente {
 	public String nome;
 	public String cpf;
 	
-	public List<Endereco> enderecos;
+	private List<Endereco> enderecos;
+	
+	public void adicionaEndereco(Endereco endereco) {
+		if (endereco == null) {
+			throw new NullPointerException("Endereco nao pode ser nulo");
+		}
+		
+		if (endereco.cep == null) {
+			throw new NullPointerException("Cep nao pode ser nulo");
+		}
+		
+		getEnderecos().add(endereco);
+	}
+	
+//	public void setEnderecos(List<Endereco> enderecos) {
+//		this.enderecos = enderecos;
+//	}
+	
+	private List<Endereco> getEnderecos() {
+		if (enderecos == null) {
+			enderecos = new ArrayList<Endereco>();
+		}
+		return enderecos;
+	}
 }
