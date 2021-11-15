b97524617b43e661c737b5978f9cc075a5cf8ecc
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-15 18:01:11 -0300

    POO - Implementação das classes Cliente e Endereco

---
+++ src/poo/modal/Endereco.java
@@ -0,0 +1,17 @@
+package poo.modal;
+
+public class Endereco {
+
+	enum TipoEndereco {
+		RESIDENCIAL, 
+		ENTREGA, 
+		TRABALHO}
+	
+	public String endereco;
+	public String numero;
+	public String complemento;
+	public String bairro;
+	public String estado;
+	public String cep;
+	
+}
